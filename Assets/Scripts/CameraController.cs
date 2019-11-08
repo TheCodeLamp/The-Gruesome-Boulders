531d82137e3bdcbe6ad7affa30fbbb97d6f03ed3
Tom Larsson <tomla035@student.liu.se>
2019-11-08 23:17:21 +0100

    Movement and camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float boostSpeed;
+    public GameObject player;
+    private float distanceX;
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if(moveHorizontal == 1f)
+        {
+            Vector3 cameraChange = new Vector3(0.01f, 0, 0);
+            cam.transform.position += cameraChange;
+        }
+        else if (moveHorizontal == -1f)
+        {
+            Vector3 cameraChange = new Vector3(-0.01f, 0, 0);
+            cam.transform.position += cameraChange;
+        }
+    }
+
+    float getDistance()
+    {
+        distanceX = player.transform.position.x - transform.position.x;
+        return distanceX;
+    }
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed;
+    public Rigidbody2D p1;
     void Start()
     {
-
+        p1 = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        p1.AddForce(movement * speed);
     }
 }
