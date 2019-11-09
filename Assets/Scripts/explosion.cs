050740c7e3bde7662d3c36fdd20acc5265525d39
Tom Larsson <tomla035@student.liu.se>
2019-11-09 22:00:00 +0100

    abilities

---
+++ Assets/Scripts/explosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public GameObject explosionobject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Instantiate(explosionobject, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+            Destroy(explosionobject);
+            
+
+      
+        }
+        if (Input.GetButtonDown("Submit"))
+        {
+            explosionobject.GetComponent<PointEffector2D>().forceMagnitude *= -1f;
+        }
+
+    }
+}
