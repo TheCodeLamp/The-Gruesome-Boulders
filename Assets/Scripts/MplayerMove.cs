c604ec6b465c9bbcfaa030acb7b5b478ce050611
Mattias <mattias.jenvald@gmail.com>
2019-11-09 11:45:36 +0100

    Player camera

---
+++ Assets/Scripts/MplayerMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MplayerMove : MonoBehaviour
+{
+    [Header("Input Variables")]
+    [Range(0.2f, 0.3f)]
+    public float moveSpeed;
+
+    public float jumpStrength;
+    
+    [HideInInspector]
+    public int moveDirX, moveDirY;
+    public CameraM camScript;
+
+    private float horizontalAxis;
+    private Vector3 deltaMove = new Vector3();
+
+    private void FixedUpdate()
+    {
+        MoveInput();
+    }
+
+    void MoveInput()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+      
+        if (horizontalAxis > 0)
+        {
+            moveDirX = 1;
+        }
+        else if (horizontalAxis < 0)
+        {
+            moveDirX = -1;
+        }
+        else
+        {
+            moveDirX = 0;
+        }
+        
+        deltaMove.x = moveDirX * moveSpeed;
+        
+        //Apply Changes
+        transform.position += deltaMove;
+        camScript.UpdateCameraPos();
+    }
+}
