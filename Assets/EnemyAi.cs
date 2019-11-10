2a6269415804710290b69ce75c5d3c2ad6903e12
Edvin Kjell <edvkj374@student.liu.se>
2019-11-10 16:17:07 +0100

    Pathfinding

---
+++ Assets/EnemyAi.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error) ;
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Patrol : MonoBehaviour
+{
+    public float startWaitTime;
+    private float waitTime;
+    public float speed;
+    public Transform[] moveSpots;
+    public int randomSpot;
+    public GameObject target;
+    public Collider2D finded;
+
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            } else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Found");
+            finded.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
+   
